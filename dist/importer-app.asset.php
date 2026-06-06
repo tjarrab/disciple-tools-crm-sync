fe4375963d07bb53003429bf0b13317ccc19c59f
Farah <dev@tjarrab.com>
2026-06-06 12:20:19 +0100

    Missed the dist

--- dist/importer-app.asset.php
+++ dist/importer-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '4b50d97125bcb1635039');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e61bc0a0673a6f6dc0e7');
